cdac3ea777c9c39ec9159dde6e1dd97519fc8d4d
Manitrisha <24eg112d18@anurag.edu.in>
2026-05-18 15:55:05 +0530

    Fix trailing slash in CORS allowed origins

--- employee-backend/server.js
+++ employee-backend/server.js
@@ -8,7 +8,11 @@ const app = exp();
 //add cors middleware
 app.use(
   cors({
-    origin: ["https://employee-git-main-manitrishas-projects.vercel.app/", "http://localhost:5173"],
+    origin: [
+      "https://employee-git-main-manitrishas-projects.vercel.app",
+      "https://employee-inky-chi.vercel.app",
+      "http://localhost:5173"
+    ],
   })
 );
 //body parser middleware
